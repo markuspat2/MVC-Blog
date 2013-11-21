1e0c66b57052ead48c2d8c0b7cfb4dbd67a6f868
Mark Patterson <markuspat2@gmail.com>
2013-11-21 14:35:32 -0800

    Update blog.php
    misspelled controller. derp.

--- blog/blog.php
+++ blog/blog.php
@@ -10,7 +10,7 @@ http://www.youtube.com/watch?v=ksMFioOxFlk&list=PL20XMsrG9BmqmM8-ftQ44vI2dfZQKxC
 I strongly recommend this tutorial. Great pace and he teaches abstraction really well. 
 */
 
-include 'controler.php';
+include 'controller.php';
 require 'db.php';
 
 session_start();
